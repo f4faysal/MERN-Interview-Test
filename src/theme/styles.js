a74cb703c34ddf6419b9ba01083ebe32c298e740
f4faysal <f4faysals@gmail.com>
2023-12-13 23:47:32 +0600

    Add Tailwind CSS and PostCSS plugins

---
+++ src/theme/styles.js
@@ -0,0 +1,17 @@
+import reactCSS from "reactcss";
+
+export const styles = reactCSS({
+  default: {
+    righticons: {
+      border: "none",
+      width: `${window.innerWidth * 0.06}px`,
+      height: `${window.innerHeight * 0.09}px`,
+      background: "none",
+      borderRadius: "0.1%",
+      outline: "none",
+      padding: "0.5%",
+    },
+   
+ 
+  },
+});
