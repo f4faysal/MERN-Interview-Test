5d7d28eab1d7142ee466dbaa7b8651204c2bea5e
f4faysal <f4faysals@gmail.com>
2023-12-14 00:04:58 +0600

    Refactor BlackBoard component

--- src/components/BlackBoard.jsx
+++ src/components/BlackBoard.jsx
@@ -1,196 +1,203 @@
 import { useState } from "react";
 
 function BlackBoard() {
-  
-  const [path, setPath] = useState([]);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [elements, setElements] = useState([]);
-  const [action, setAction] = useState("none");
-
-
   const updateElement = (
-     index,
-     x1,
-     y1,
-     x2,
-     y2,
-     toolType,
-     strokeWidth,
-     strokeColor
-   ) => {
-     const updatedElement = createElement(
-       index,
-       x1,
-       y1,
-       x2,
-       y2,
-       toolType,
-       strokeWidth,
-       strokeColor
-     );
-     const elementsCopy = [...elements];
-     elementsCopy[index] = updatedElement;
-     setElements(elementsCopy);
-   };
- 
+    index,
+    x1,
+    y1,
+    x2,
+    y2,
+    toolType,
+    strokeWidth,
+    strokeColor
+  ) => {
+    const updatedElement = createElement(
+      index,
+      x1,
+      y1,
+      x2,
+      y2,
+      toolType,
+      strokeWidth,
+      strokeColor
+    );
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updatedElement;
+    setElements(elementsCopy);
+  };
 
-   const handleMouseDown = (e) => {
-     const { clientX, clientY } = e;
-     const canvas = document.getElementById("canvas");
-     const context = canvas.getContext("2d");
- 
-     if (toolType === "selection") {
-       const element = getElementAtPosition(clientX, clientY, elements);
-       if (element) {
-         const offsetX = clientX - element.x1;
-         const offsetY = clientY - element.y1;
-         setSelectedElement({ ...element, offsetX, offsetY });
-         if (element.position === "inside") {
-           setAction("moving");
-         } else {
-           setAction("resize");
-         }
-       }
-     } else if (toolType === "eraser") {
-       setAction("erasing");
- 
-       checkPresent(clientX, clientY);
-     } else {
-       const id = elements.length;
-       if (toolType === "pencil" || toolType === "brush") {
-         setAction("sketching");
-         setIsDrawing(true);
- 
-         const newColour = colorWidth.hex;
-         const newLinewidth = width;
-         const transparency = toolType === "brush" ? "0.1" : "1.0";
-         const newEle = {
-           clientX,
-           clientY,
-           newColour,
-           newLinewidth,
-           transparency,
-         };
-         setPoints((state) => [...state, newEle]);
- 
-         context.strokeStyle = newColour;
-         context.lineWidth = newLinewidth;
-         context.lineCap = 5;
-         context.moveTo(clientX, clientY);
-         context.beginPath();
-       } else {
-         setAction("drawing");
-         const newColour = colorWidth.hex;
-         const newWidth = shapeWidth;
-         const element = createElement(
-           id,
-           clientX,
-           clientY,
-           clientX,
-           clientY,
-           toolType,
-           newWidth,
-           newColour
-         );
- 
-         setElements((prevState) => [...prevState, element]);
-         setSelectedElement(element);
-       }
-     }
-   };
+  
 
   const handleMouseDown = (e) => {
-     const { clientX, clientY } = e;
-     const canvas = document.getElementById("canvas");
-     const context = canvas.getContext("2d");
- 
-     if (toolType === "selection") {
-       const element = getElementAtPosition(clientX, clientY, elements);
-       if (element) {
-         const offsetX = clientX - element.x1;
-         const offsetY = clientY - element.y1;
-         setSelectedElement({ ...element, offsetX, offsetY });
-         if (element.position === "inside") {
-           setAction("moving");
-         } else {
-           setAction("resize");
-         }
-       }
-     } else if (toolType === "eraser") {
-       setAction("erasing");
- 
-       checkPresent(clientX, clientY);
-     } else {
-       const id = elements.length;
-       if (toolType === "pencil" || toolType === "brush") {
-         setAction("sketching");
-         setIsDrawing(true);
- 
-         const newColour = colorWidth.hex;
-         const newLinewidth = width;
-         const transparency = toolType === "brush" ? "0.1" : "1.0";
-         const newEle = {
-           clientX,
-           clientY,
-           newColour,
-           newLinewidth,
-           transparency,
-         };
-         setPoints((state) => [...state, newEle]);
- 
-         context.strokeStyle = newColour;
-         context.lineWidth = newLinewidth;
-         context.lineCap = 5;
-         context.moveTo(clientX, clientY);
-         context.beginPath();
-       } else {
-         setAction("drawing");
-         const newColour = colorWidth.hex;
-         const newWidth = shapeWidth;
-         const element = createElement(
-           id,
-           clientX,
-           clientY,
-           clientX,
-           clientY,
-           toolType,
-           newWidth,
-           newColour
-         );
- 
-         setElements((prevState) => [...prevState, element]);
-         setSelectedElement(element);
-       }
-     }
-   };
- 
- 
+    const { clientX, clientY } = e;
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    if (toolType === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if (element) {
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({ ...element, offsetX, offsetY });
+        if (element.position === "inside") {
+          setAction("moving");
+        } else {
+          setAction("resize");
+        }
+      }
+    } else if (toolType === "eraser") {
+      setAction("erasing");
+
+      checkPresent(clientX, clientY);
+    } else {
+      const id = elements.length;
+      if (toolType === "pencil" || toolType === "brush") {
+        setAction("sketching");
+        setIsDrawing(true);
+
+        const newColour = colorWidth.hex;
+        const newLinewidth = width;
+        const transparency = toolType === "brush" ? "0.1" : "1.0";
+        const newEle = {
+          clientX,
+          clientY,
+          newColour,
+          newLinewidth,
+          transparency,
+        };
+        setPoints((state) => [...state, newEle]);
+
+        context.strokeStyle = newColour;
+        context.lineWidth = newLinewidth;
+        context.lineCap = 5;
+        context.moveTo(clientX, clientY);
+        context.beginPath();
+      } else {
+        setAction("drawing");
+        const newColour = colorWidth.hex;
+        const newWidth = shapeWidth;
+        const element = createElement(
+          id,
+          clientX,
+          clientY,
+          clientX,
+          clientY,
+          toolType,
+          newWidth,
+          newColour
+        );
+
+        setElements((prevState) => [...prevState, element]);
+        setSelectedElement(element);
+      }
+    }
+  };
+
+  const handleMouseMove = (e) => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const { clientX, clientY } = e;
+    if (toolType === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      e.target.style.cursor = element
+        ? cursorForPosition(element.position)
+        : "default";
+    }
+    if (action === "erasing") {
+      checkPresent(clientX, clientY);
+    }
+    if (action === "sketching") {
+      if (!isDrawing) return;
+      const colour = points[points.length - 1].newColour;
+      const linewidth = points[points.length - 1].newLinewidth;
+      const transparency = points[points.length - 1].transparency;
+      const newEle = { clientX, clientY, colour, linewidth, transparency };
+
+      setPoints((state) => [...state, newEle]);
+      var midPoint = midPointBtw(clientX, clientY);
+      context.quadraticCurveTo(clientX, clientY, midPoint.x, midPoint.y);
+      context.lineTo(clientX, clientY);
+      context.stroke();
+    } else if (action === "drawing") {
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      elements[index].strokeColor = colorWidth.hex;
+      elements[index].strokeWidth = shapeWidth;
+      updateElement(
+        index,
+        x1,
+        y1,
+        clientX,
+        clientY,
+        toolType,
+        shapeWidth,
+        colorWidth.hex
+      );
+    } else if (action === "moving") {
+      const {
+        id,
+        x1,
+        x2,
+        y1,
+        y2,
+        type,
+        offsetX,
+        offsetY,
+        shapeWidth,
+        strokeColor,
+      } = selectedElement;
+      const offsetWidth = x2 - x1;
+      const offsetHeight = y2 - y1;
+      const newX = clientX - offsetX;
+      const newY = clientY - offsetY;
+      updateElement(
+        id,
+        newX,
+        newY,
+        newX + offsetWidth,
+        newY + offsetHeight,
+        type,
+        shapeWidth,
+        strokeColor
+      );
+    } else if (action === "resize") {
+      const { id, type, position, ...coordinates } = selectedElement;
+      const { x1, y1, x2, y2 } = resizedCoordinates(
+        clientX,
+        clientY,
+        position,
+        coordinates
+      );
+      updateElement(id, x1, y1, x2, y2, type, shapeWidth, colorWidth.hex);
+    }
+  };
   const handleMouseUp = () => {
-     if (action === "resize") {
-       const index = selectedElement.id;
-       const { id, type, strokeWidth, strokeColor } = elements[index];
-       const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
-       updateElement(id, x1, y1, x2, y2, type, strokeWidth, strokeColor);
-     } else if (action === "drawing") {
-       const index = selectedElement.id;
-       const { id, type, strokeWidth } = elements[index];
-       const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
-       updateElement(id, x1, y1, x2, y2, type, strokeWidth, colorWidth.hex);
-     } else if (action === "sketching") {
-       const canvas = document.getElementById("canvas");
-       const context = canvas.getContext("2d");
-       context.closePath();
-       const element = points;
-       setPoints([]);
-       setPath((prevState) => [...prevState, element]); //tuple
-       setIsDrawing(false);
-     }
-     setAction("none");
-   };
- 
+    if (action === "resize") {
+      const index = selectedElement.id;
+      const { id, type, strokeWidth, strokeColor } = elements[index];
+      const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
+      updateElement(id, x1, y1, x2, y2, type, strokeWidth, strokeColor);
+    } else if (action === "drawing") {
+      const index = selectedElement.id;
+      const { id, type, strokeWidth } = elements[index];
+      const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
+      updateElement(id, x1, y1, x2, y2, type, strokeWidth, colorWidth.hex);
+    } else if (action === "sketching") {
+      const canvas = document.getElementById("canvas");
+      const context = canvas.getContext("2d");
+      context.closePath();
+      const element = points;
+      setPoints([]);
+      setPath((prevState) => [...prevState, element]); //tuple
+      setIsDrawing(false);
+    }
+    setAction("none");
+  };
+
+
   return (
     <div>
-    <canvas
+      <canvas
         id="canvas"
         className="App"
         width={window.innerWidth}
